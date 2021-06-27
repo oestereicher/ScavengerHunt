e3652f4591626a6fe8516d14a26ccfd25bd5deb1
oestereicher <brandon.oestereicher@gmail.com>
2021-06-27 11:54:08 -0500

    added sign in button

--- Journal/AppDelegate.swift
+++ Journal/AppDelegate.swift
@@ -29,9 +29,10 @@
 import UIKit
 import CoreLocation
 import UserNotifications
+import GoogleSignIn
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
   static let geoCoder = CLGeocoder()
 
   var window: UIWindow?
@@ -75,8 +76,43 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
       UserDefaults.standard.set(true, forKey: "alreadyLaunched")
       UserDefaults.standard.set(0, forKey: "maxUnlockedIndex")
     }
+    
+    // Initialize sign-in
+    GIDSignIn.sharedInstance()?.clientID = "945241195780-60m2nqcbkltmm0boqvc5ujl8qsat54jd.apps.googleusercontent.com"
+    GIDSignIn.sharedInstance()?.delegate = self
 
     return true
+  }
+  
+  @available(iOS 9.0, *)
+  func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+    return GIDSignIn.sharedInstance().handle(url)
+  }
+  
+  func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!,
+            withError error: Error!) {
+    if let error = error {
+      if (error as NSError).code == GIDSignInErrorCode.hasNoAuthInKeychain.rawValue {
+        print("The user has not signed in before or they have since signed out.")
+      } else {
+        print("\(error.localizedDescription)")
+      }
+      return
+    }
+    // Perform any operations on signed in user here.
+//    let userId = user.userID                  // For client-side use only!
+//    let idToken = user.authentication.idToken // Safe to send to the server
+//    let fullName = user.profile.name
+//    let givenName = user.profile.givenName
+//    let familyName = user.profile.familyName
+//    let email = user.profile.email
+    // ...
+  }
+  
+  func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!,
+            withError error: Error!) {
+    // Perform any operations when the user disconnects from app here.
+    // ...
   }
   
   func createLocations(_ info: [SimpleLocation]) -> [Location] {
